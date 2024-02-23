a14d5d1d11e7492994df8c79a6339f8dfcab3bc2
Tayebwa-ian <tayebwaian0@gmail.com>
2024-02-23 13:01:40 -0800

    correct errors

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,16 @@
-# installing puppet link
-package { 'puppet-lint':
-    ensure   => '2.1.1',
-    provider => 'gem',
+# installing flask
+package { 'python3':
+  ensure => 'installed',
+}
+
+package { 'python3-pip':
+  ensure => 'installed',
+  require => Package['python3'],
+}
+
+# Install Flask using pip3 with the desired version
+package { 'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
+  require  => Package['python3-pip'],
 }
