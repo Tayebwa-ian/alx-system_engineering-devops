e8c88e704163c506becc8d9b47e26d4bd702da5e
Tayebwa-ian <tayebwaian0@gmail.com>
2024-02-23 13:43:22 -0800

    correct errors

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,16 +1,5 @@
-# installing flask
-package { 'python3':
-  ensure => 'installed',
-}
-
-package { 'python3-pip':
-  ensure => 'installed',
-  require => Package['python3'],
-}
-
 # Install Flask using pip3 with the desired version
 package { 'flask':
   ensure   => '2.1.0',
   provider => 'pip3',
-  require  => Package['python3-pip'],
 }
