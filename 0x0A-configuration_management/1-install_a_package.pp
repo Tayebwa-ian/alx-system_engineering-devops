0416237a97fc774d1bfd12548ebfd5fc36cf20f1
Tayebwa-ian <tayebwaian0@gmail.com>
2024-02-23 22:57:40 -0800

    Installiing flask with Puppet

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,23 @@
+# Install python3 and pip3
+package { 'python3':
+  ensure => 'installed',
+}
+
+package { 'python3-pip':
+  ensure  => 'installed',
+  require => Package['python3'],
+}
+
+# Install Werkzeug using pip
+package { 'Werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip3',
+  require  =>Package['python3-pip'],
+}
+
 # Install Flask using pip3 with the desired version
 package { 'flask':
   ensure   => '2.1.0',
   provider => 'pip3',
+  require  => Package['Werkzeug'],
 }
