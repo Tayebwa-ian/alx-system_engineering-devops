45abbc2e4437240eb80f7a4bccb33bf56ac25ae2
Tayebwa-ian <tayebwaian0@gmail.com>
2024-02-25 07:06:32 -0800

    Make SSH client configuration using puppet

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,5 +1,7 @@
 # SSH Client configuration
-file { 'home/3015f484ce90/.ssh/config':
+file { '~/.ssh/config':
   ensure  => present,
+  owner   => 'root',
+  group   => 'root',
   content => "Host Ubuntu\n\tIdentityFile ~/.ssh/school\n\tPasswordAuthentication no",
 }
