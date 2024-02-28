871e4e37a2d5230d183d1edfc699ea6991be7c13
Tayebwa-ian <tayebwaian0@gmail.com>
2024-02-28 05:58:35 -0800

    Write a puppet manifest

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -6,8 +6,8 @@ exec {'install_update':
 }
 
 package {'nginx':
-  ensure   => 'present',
-  required => Exec['install_update'],
+  ensure  => 'present',
+  require => Exec['install_update'],
 }
 
 exec {'change_owner':
@@ -16,20 +16,29 @@ exec {'change_owner':
 }
 
 exec {'owner_change':
-  command  => 'sudo chown -R "$USER":"$USER" /etc/nginx'
+  command  => 'sudo chown -R "$USER":"$USER" /etc/nginx',
   provider => shell,
 }
 
 exec {'Hello':
   command  => 'echo "Hello World!" | sudo tee /var/www/html/index.html',
   provider => shell,
+  require  => Exec['change_owner'],
 }
 
-exec {'sudo sed -i "48i\\\tlocation /redirect_me {\n\t\treturn 301 https://www.youtube.com/watch?v=dQw4w9WgXcQ permanent;\n\t}\n\terror_page 404 /404.html;\n\tlocation = /404.html {\n\t\troot /usr/share/nginx/html;\n\t\tinternal;\n\t}\n"':
+exec {'404_message':
+  command  => 'echo "Ceci n\'est pas une page" | sudo tee /usr/share/nginx/html/404.html',
   provider => shell,
+}
+
+exec {'configure':
+  command  => 'sudo sed -i "48i\\	location /redirect_me {\n\t\treturn 301 https://www.youtube.com/watch?v=dQw4w9WgXcQ;\n\t}\n\n\terror_page 404 /404.html;\n\tlocation = /404.html {\n\t\troot /usr/share/nginx/html;\n\t\tinternal;\n\t}\n" /etc/nginx/sites-available/default',
+  provider => shell,
+  require  => Exec['owner_change'],
 }
 
 exec {'nginx_restart':
   command  => 'sudo service nginx restart',
   provider => shell,
+  require  => Exec['configure'],
 }
